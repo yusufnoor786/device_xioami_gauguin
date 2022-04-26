3509e681500912e5593cdfeb83b49e444c150568
Lucchetto <lucchetto.tie@live.com>
2022-04-25 20:56:35 -0400

    gauguin: Create dummy libqti-perfd-clienti
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- device.mk
+++ device.mk
@@ -384,7 +384,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    libqti-perfd-client
 
 # Power
 PRODUCT_PACKAGES += \
