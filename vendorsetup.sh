89c2b27f0435153fb90dd75d885a43b861735ca3
Zabuka_zuzu <zuzu950444@gmail.com>
2023-09-07 08:03:13 +0000

    Added Vendorsetup file *vendor *kernel *hardware/xiaomi

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/yusufnoor786/vendor_xiaomi_gauguin.git -b derp-13 vendor/xiaomi/gauguin
+
+git clone https://github.com/yusufnoor786/kernel_xiaomi_gauguin.git -b topaz kernel/xiaomi/gauguin
+
+git clone https://github.com/PixelExperience/hardware_xiaomi.git -b thirteen hardware/xiaomi
