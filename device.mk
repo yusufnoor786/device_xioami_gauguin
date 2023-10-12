5535bdd86b360d39ad0dfed4c9fb4cbfeccdc097
adi8900 <adrianszymanski242@gmail.com>
2023-10-12 17:57:54 +0000

    gauguin: Patch blobs to use full protobuf "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libwvhidl.so "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/sensors.ssc.so "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/bin/sensors.qti "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libsensorcal.so "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,9 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.mi.node.watermark.so)
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib64/libssc.so | vendor/lib64/libsnsapi.so | vendor/lib64/libsensorcal.so | vendor/lib64/libsnsdiaglog.so | vendor/lib64/sensors.ssc.so | vendor/bin/sensors.qti | vendor/lib64/libwvhidl.so | vendor/lib/libssc.so | vendor/lib/libsnsapi.so | vendor/lib/libsensorcal.so | vendor/lib/libsnsdiaglog.so | vendor/lib/sensors.ssc.so )
+        "${PATCHELF} --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
