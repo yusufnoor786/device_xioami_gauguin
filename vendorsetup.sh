78ebd174c92be62b18787684ea9a06fb34aeab77
ShukakuZa <yusufzuzu787@gmail.com>
2023-10-13 11:20:59 +0000

    gauguin: updated vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/yusufnoor786/vendor_xiaomi_gauguin.git -b derp-13 vendor/xiaomi/gauguin
+git clone https://github.com/yusufnoor786/vendor_xiaomi_gauguin.git -b 14 vendor/xiaomi/gauguin
 
-git clone https://github.com/yusufnoor786/kernel_xiaomi_gauguin.git -b topaz kernel/xiaomi/gauguin
+git clone https://github.com/xiaomi-sm7225/android_kernel_xiaomi_gauguin.git -b lineage-20 kernel/xiaomi/gauguin
 
-git clone https://github.com/PixelExperience/hardware_xiaomi.git -b thirteen hardware/xiaomi
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git  -b arrow-13.1 hardware/xiaomi
