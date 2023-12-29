20aaf0c4dee0469802b49f34058903dbe3331363
ShukakuZa <yusufzuzu787@gmail.com>
2023-12-29 19:41:12 +0000

    gauguin: Vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/yusufnoor786/vendor_xiaomi_gauguin.git -b udc vendor/xiaomi/gauguin
+git clone https://github.com/yusufnoor786/kernel_xiaomi_gauguin.git -b udc kernel/xiaomi/gauguin
